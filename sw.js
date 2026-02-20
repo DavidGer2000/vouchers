dc38f6a7d8f15ef7b0c08aeb43047a5c13c79d1b
DavidGer2000 <164003542+DavidGer2000@users.noreply.github.com>
2026-02-20 14:02:39 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'vouchers-v4';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
